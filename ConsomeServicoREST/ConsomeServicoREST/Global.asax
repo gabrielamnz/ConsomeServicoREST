a1147d009df7b3c6faf5792cc2f716bc71ea56fc
gabrielamnz <gmnzfranco@gmail.com>
2019-06-24 22:10:06 -0300

    Criando projeto

---
+++ ConsomeServicoREST/ConsomeServicoREST/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ConsomeServicoREST.MvcApplication" Language="C#" %>
---
+++ ConsomeServicoREST/ConsomeServicoREST/Models/DTO/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConsomeServicoREST.Models.DTO
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Body { get; set; }
+    }
+}
